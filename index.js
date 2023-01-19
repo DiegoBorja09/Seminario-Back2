ed4bcaf9717a52dfe99b098966040a0cf3f2b2f1
DiegoBorja09 <diegoborja09@gmail.com>
2023-01-19 18:11:07 -0500

    final

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const { port } = require("./config")
 const {connection} = require("./config/db")
+const motosModel=require("./models/motos")
+const motosservices= require("./services/motos")
+
 
 //Importar Routers
-
-const routermotos= require("./routes/motos")
 
 connection()
 const app = express()
@@ -12,8 +13,6 @@ const app = express()
 app.use(express.json())
 
 //utilizamos Routers
-
-routermotos(app)
 
 app.get("/",(req,res)=>{
     return res.json({
@@ -25,3 +24,38 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log("Listening: http://localhost:"+port)
 })
+ 
+const motserv= new motosservices()
+ app.get("/motos",async(req,res)=>{ 
+
+    const motos = await motserv.getAll()
+
+    return res.json(motos)
+            
+        
+})
+app.put("/update:id",async(req,res)=>{
+
+    const motos = await motserv.update(req.params.id,req.body)
+
+    return res.json(motos)
+
+        
+})
+app.delete("/delete:id",async(req,res)=>{
+
+    const motos = await motserv.delete(req.params.id)
+
+    return res.json(motos)
+})
+
+app.post("/create",async(req,res)=>{
+
+    const motos = await motserv.create(req.body)
+
+    return res.json(motos)
+
+        
+})
+
+
